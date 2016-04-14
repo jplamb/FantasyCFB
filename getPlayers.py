d622a0a21cd14c63ddee1d5ee56da03edc50e3f3
jplamb <j.lamb13@gmail.com>
2016-04-13 22:10:42 -0400

    Fixed bug where player id not captured correctly, removed add player to table method, added comments

--- getPlayers.py
+++ getPlayers.py
@@ -8,6 +8,7 @@ from lxml import html
 from bs4 import BeautifulSoup, Tag
 import requests
 import re
+from dbConn import db_execute
 
 # Retrieve all power five conference teams' rosters
 # Inputs: URL string to page with all college football teams' rosters
@@ -64,9 +65,11 @@ def get_team_roster(url):
 	player_info = [[],[],[],[]]
 	
 	# For each player, grab their name, ID, and url
+	count  = 0
 	for player in players_soup:
 		if player is not None and player.string is not None:
-			player_info.append([player.string, re.search("id\/([^\s]+)\/",unicode(players_soup[0])).group(1),player['href']])
+			player_info.append([player.string, re.search("id\/([^\s]+)\/",unicode(players_soup[count])).group(1),player['href']])
+			count +=1
 		#players_name.append(player.string)
 		#players_id.append(re.search("id\/([^\s]+)\/",unicode(players_soup[0])).group(1))
 		#players_url.append(player['href'])
@@ -78,51 +81,60 @@ def get_team_roster(url):
 	player_info = filter(None, player_info)
 	player_info = [x for x in player_info if x!= None and x != []]
 	
+	# Get team name from url
 	team = url.split('/')[-1]
 	
+	# Insert player into player table in DB
 	for player in player_info:	
 		add_to_player_table(player, team)
 	
 	return player_info
 
+# Adds player to 'player' table
+# inputs: player_info as array of player (str), ID (int), url (str) and team (str) 
 def add_to_player_table(player_info, team):
-		name = player_info[0]
-		id = player_info[1]
-		url = player_info[2]
 		
-		sql = """
-				select (1)
-				from players
-				where player_id = %s
-				""" % (self.id)
-		
-		if db_execute(sql):
-			update_sql = """
-				update players
-				set 
-				name = '%s',
-				url = '%s',
-				team = '%s'
-				where 
-				player_id = %s
-				""" % (name, url, team, id)
-			
-			db_execute(update_sql)
-		
-		else:
-			insert_sql = """
-				insert into players (
-				player_id,
-				name,
-				url,
-				team)
-				values (
-				%s,
-				'%s',
-				'%s',
-				'%s')
-				""" % (id, name, url, team)
-			
-			db_execute(insert_sql)
+	# strip names of special characters
+	name = re.sub("[-.']", "", player_info[0])
+	player_id = player_info[1]
+	# escape apostrophes in url
+	url = re.sub("[']", "''", player_info[2])
+	
+	# check if player already exists in db
+	sql = """
+			select (1)
+			from players
+			where player_id = %s
+			""" % (player_id)
+	
+	# run either update or insert if row exists or not
+	if db_execute(sql):
+		update_sql = """
+			update players
+			set 
+			name = '%s',
+			url = '%s',
+			team = '%s'
+			where 
+			player_id = %s
+			""" % (name, url, team, player_id)
+
+		db_execute(update_sql)
+
+	else:
+		insert_sql = """
+			insert into players (
+			player_id,
+			name,
+			url,
+			team)
+			values (
+			%s,
+			'%s',
+			'%s',
+			'%s')
+			""" % (player_id, name, url, team)
+
+		db_execute(insert_sql)
 				
 
