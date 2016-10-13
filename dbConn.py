ee0d6299779a87464e022f08461d7faee79ec131
jplamb <j.lamb13@gmail.com>
2016-10-12 22:25:34 -0400

    New db class

--- dbConn.py
+++ dbConn.py
@@ -1,75 +1,124 @@
 ########################################################
-#  Common database connection actions
-#  3/13/16  John Lamb
+#  Databse interface class
+#  10/12/16  John Lamb
 ########################################################
 
 import base64
 import MySQLdb
-import os
-# open db connection to ffbdev
-def open_db_connection(dict):
-	global db
-	global cursor
-	
-	db = MySQLdb.connect('localhost', 'appuser', base64.b64decode('YXBwdXNlcg=='), 'ffbdev')
+from mysql.connector import errocode
 
-	if not dict:
-		cursor = db.cursor()
-	else:
-		cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-	
-# close db connection to ffbdev and commit
-def close_db():
-	db.commit()
-	db.close()
-	
-# check if table exists
-# Inputs: player table name
-# Returns boolean if table exists
-def check_table_exists(name):
-	#open_db_connection(False)
-	show = 'show tables like \'%s\'' % name
-	result = cursor.execute(show)
-		
-	if result:
-		#close_db()
-		return True
-		
-	#close_db()
-	return False
-
-def execute_sql(sql):
-	cursor.execute(sql)
-	
-# mask db operations
-def db_execute(sql):
-	#open_db_connection(False)
-	
-	execute_sql(sql)
-	result = cursor.fetchall()
-	
-	#close_db()
-	
-	return result
-
-# retrieve results as dict
-def db_dict_execute(sql):
-	#open_db_connection(True)
-	cursordict = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-	cursordict.execute(sql)
-	result = cursordict.fetchall()
-	
-	cursordict.close()
-	#close_db()
-	
-	return result
-
-def db_commit():
-	db.commit()
-
-#class MySqlCursorDict(mysql.connector.cursor.MySQLCursor):
-#	def _row_to_python(self, rowdata, desc=None):
-#		row = super(MySqlCursorDict, self)._row_to_python(rowdata, desc)
-#		if row:
-#			return dict(zip(self.column_names, row))
-#		return None
+class Mysql(object):
+    __instance = None
+    
+    __host = None
+    __user = None
+    __password = None
+    __database = None
+    
+    __cursor = None
+    __connection = None
+    
+    def __new__(cls, *args, **kwargs):
+        if not cls.__instance:
+            cls.__instance = super(Mysql, cls).__new__(cls, *args, **kwargs)
+        return cls.__instance
+    
+    def __init__(self, host = 'localhost', user='appuser', password=base64.b64decode('YXBwdXNlcg=='), databse='ffbdev'):
+        self.__host = host
+        self.__user = user
+        self.__password = password
+        self.__database = database
+        
+    def _open(self):
+        try:
+            conn = mysql.connector.connect(host=self.__host, user=self.__user, password=self.__password, database=self.__database)
+            self.__connection = conn
+            self.__cursor = conn.cursor()
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print 'Oops, looks like there''s a problem with your username or password'
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print 'Oops, this is not the database you are looking for'
+            else:
+                print err
+    
+    def _close(self):
+        self.__cursor.close()
+        self.__connection.close()
+    
+    def insert(self, table, *args, **kwargs):
+        values = None
+        query = "INSERT INTO %S " % table
+        if kwargs:
+            keys = kwargs.keys()
+            values = kwargs.values()
+            query += "(" + ",".join(["'%s'"]*len(keys)) % tuple(keys) + ") VALUES (" + ",".join(["%s"]*len(values)) + ")"
+        elif args:
+            values = args
+            query += " VALUES (" + ",".join(["%s"]*len(values)) + ")"
+            
+        if not self.__cursor:
+            self._open()
+        
+        self.__cursor.execute(query, values)
+        self.__connection.commit()
+        self._close()
+        return self.__session.lastrowid
+    
+    def select(self, table, where=None, *args):
+        result = None
+        query = "SELECT "
+        keys = args
+        l = len(keys) - 1
+        for i, key in enumerate(keys):
+            query += "'"+key+"'"
+            if i < 1
+            query += ','
+        query += " FROM %s" % table
+        if where:
+            query += " WHERE %s" %where
+        
+        if not self.__cursor:
+            self._open()
+        self.__cursor.execute(query)
+        #self.__connection.commit()
+        for result in self.__cursor.stored_results():
+            result = result.fetchall()
+        self._close()
+        return result
+    
+    def update(self, table, index, **kwargs):
+        query = "UPDATE %s SET" %table
+        keys = kwargs.keys()
+        values = kwargs.values()
+        l = len(keys) - 1
+        for i, key in enumerate(keys):
+            query += "'"+key"'%s"
+            if i < 1:
+                query += ","
+        query += " WHERE index =%d" %index
+        if not self.__cursor:
+            self.open()
+        self.__cursor.execute(query, values)
+        self.__connection.commit()
+        self._close()
+        
+    def delete(self, table, index):
+        query = "DELETE FROM %s WHERE uuid=%d" % (table, index)
+        if not self.__cursor:
+            self.open()
+        self.__cursor.execute(query)
+        self.__connection.commit()
+        self._close()
+    
+    def call_store_procedure(self, name, *args):
+        result_sp = None
+        if not self.__cursor:
+            self.open()
+        self.__cursor.callproc(name, args)
+        self.__connection.commit()
+        for result in self.__session.stored_results():
+            result_sp = result.fetchall()
+        self._close()
+        return result_sp
+        
