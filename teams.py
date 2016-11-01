ed1195005f0ceb75718c9f1eaa5b7a14038cebf1
jplamb <j.lamb13@gmail.com>
2016-10-31 20:20:42 -0400

    Retooled teams to use new dbclass

--- teams.py
+++ teams.py
@@ -3,59 +3,35 @@ from lxml import html
 from bs4 import BeautifulSoup
 import requests
 
-# Crete table
-# inputs: table_name as string
-def create_table():		
-
-    create_sql = """
-            create table teams (
-            team varchar(20) not null,
-            team_id int not null,
-            primary key(team)
-            )"""
-            
-    db_execute(create_sql)
-
 # Handle schedule updates
 # input date as string, opp as string, status as string, and time as string
 def record_team(team_name, team_id):
-    if not check_table_exists('teams'):
-        create_table()
-        
-    row_exists_sql = """
-        select (1)
-        from teams
-        where team_id = %s
-        """ % (team_id)
+    if not __conn.call_store_procedure('check_table_exists', 'teams'):
+        __conn.call_store_procedure('create_teams')
     
-    if db_execute(row_exists_sql):
-        update_team(team_name, team_id)
+    row_where = 'team_id = %s'%(team_id)
+    values = {}
+    values['team'] = team_name
+    values['team_id'] = team_id
+    
+    if __conn.select('teams', row_where, "'x'"):
+        update_team(**values)
     else:
-        insert_team(team_name, team_id)
+        insert_team(**values)
 
 # Add team to table      
-def insert_team(team_name, team_id):
-    insert_sql = """
-            insert into teams (
-            team,
-            team_id
-            )
-            values (
-            '%s',
-            %s
-            )
-            """ %(team_name, team_id)
-    print insert_sql
-    db_execute(insert_sql)
+def insert_team(**kwargs):
+    __conn.insert('teams', **kwargs)
 
 # Update team row 
-def update_team(team_name, team_id):
+def update_team(**kwargs):
+    values = kwargs
+    
+    update_where = 'team_id = %s'%(values['team_id'])
+    
+    values.pop('team_id', 0)
+    
+    __conn.update('teams', update_where, **values)
 
-    update_sql = """
-            update teams 
-            set 
-            team = '%s'
-            where
-            team_id = %s 
-            """ % (team_name, team_id)				
-    db_execute(update_sql)
+    
+__conn = Mysql()
